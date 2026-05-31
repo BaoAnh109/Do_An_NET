643632b437e6bd5c45b7e0dd4e2cc9b0ffedf696
BaoAnh109 <anh011009@gmail.com>
2026-05-31 17:42:36 +0700

    feat: rp file

--- CoffeeTea/Models/QLCoffeeTeaModel.Designer.cs
+++ CoffeeTea/Models/QLCoffeeTeaModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Leebao\Desktop\Do_An_NET\CoffeeTea\Models\QLCoffeeTeaModel.edmx'. 
+﻿// T4 code generation is enabled for model 'F:\NET\Do_An_NET\CoffeeTea\Models\QLCoffeeTeaModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
